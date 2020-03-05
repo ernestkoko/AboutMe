6e1b1dac0dd9ff712821739a898d546aed892fb6
ernestkoko <ernesteferetin@yahoo.com>
2020-03-05 16:23:55 +0100

    Added some back end code to hide the Done button when clicked and hide the keyboard also

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,12 +1,35 @@
  package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.content.getSystemService
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+        addNickname(it)}
+    }
+    private fun addNickname(view: View){
+        val editText = findViewById<EditText>(R.id.nick_name_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        // Hide the keyboard when done
+        val inn = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inn.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
